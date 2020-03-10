b90d549ef837ddae6bf494cf9c9eb146387969fd
Pufferfish3000 <mattbyrnes18@gmail.com>
2020-03-10 16:27:39 -0600

    autonomuis fixes

--- src/main/java/frc/robot/autonomous/A1.java
+++ src/main/java/frc/robot/autonomous/A1.java
@@ -26,7 +26,7 @@ public class A1 extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_driveTrain.goBackwards();
+    m_driveTrain.goForward();
     a1Time++; // increments a1Time every time it executes
   }
 
@@ -39,7 +39,7 @@ public class A1 extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    if (a1Time >= 50) { // checks to see if the program has executes 150 times
+    if (a1Time >= 80) { // checks to see if the program has executes 150 times
       return true;
     } else {
       return false;
